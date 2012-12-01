e3641a2dafe85387e6d26bc3c1d48018410b88e0
Derek Tia <dtia05@gmail.com>
2012-12-01 15:29:12 -0800

    fix search query

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,13 @@
 class Restaurant < ActiveRecord::Base
   attr_accessible :city, :name
+  validates :name, :city, :presence => true
+  
+  def self.create_restaurant(restaurant, city, cuisine)
+    new_restaurant = Restaurant.new
+    new_restaurant.name = restaurant
+    new_restaurant.city = city
+    new_restaurant.cuisine = cuisine
+    new_restaurant.save
+    return new_restaurant.id
+  end
 end
