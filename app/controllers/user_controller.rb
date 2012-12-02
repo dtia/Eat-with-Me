d51ccadfb59703484d398c43be4ad2f3c440c3ce
Derek Tia <dtia05@gmail.com>
2012-12-01 17:42:47 -0800

    fb api integration

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+  def create
+    render :nothing => true
+    user_info_json = params[:userInfo]
+    
+    user_info = ActiveSupport::JSON.decode(user_info_json)
+    user_id = user_info["uid"]
+    name = user_info["name"]
+    sex = user_info["sex"]
+    profile_pic = user_info["pic_square"]
+    meeting_sex = user_info["sex"]
+    
+    User.create_user(name, city, sex)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ RestaurantDate::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :user
 
   # Sample resource route with options:
   #   resources :products do
